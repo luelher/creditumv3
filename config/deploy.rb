f5e0fe8b3b287317040479cb7fd12402c88759f8
Luis Hernández <luelher@gmail.com>
2014-06-25 14:45:23 -0430

    Error en historial crediticio

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,37 +34,57 @@ set :deploy_to, '/home/admin/creditumv3'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :rvm_ruby_string, :local              # use the same ruby as used locally for deployment
+set :rvm_autolibs_flag, "read-only"
+
+
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
+      run "mkdir -p #{release_path}/tmp" unless test("[ -d #{release_path}/tmp ]")
       execute :touch, release_path.join('tmp/restart.txt')
+      execute "sudo service apache2 restart"
     end
   end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within release_path do
-        execute :rake, 'cache:clear'
+  desc "link config files"
+  task :link_config_files do
+    on roles(:app) do
+      if test("[ -f #{shared_path}/database.yml ]")
+        execute :ln, '-s', "#{shared_path}/database.yml", "#{release_path}/config/database.yml"
+      else
+        puts "No existe archivo database.yml en #{shared_path}"
       end
+      if test("[ -f #{shared_path}/secret_token.rb ]")
+        execute :ln, '-s', "#{shared_path}/secret_token.rb", "#{release_path}/config/initializers/secret_token.rb"
+      else
+        puts "No existe archivo database.yml en #{shared_path}"
+      end
+      # if test("[ -d #{shared_path}/assets ]")
+      #   execute :ln, '-s', "#{shared_path}/assets", "#{release_path}/public/assets"
+      # else
+      #   puts "No existe archivo database.yml en #{shared_path}"
+      # end
     end
   end
-
 
   desc "Check that we can access everything"
   task :check_write_permissions do
     on roles(:all) do |host|
       if test("[ -w #{fetch(:deploy_to)} ]")
-        info "#{fetch(:deploy_to)} is writable on #{host}"
+        puts "#{fetch(:deploy_to)} is writable on #{host}"
       else
         error "#{fetch(:deploy_to)} is not writable on #{host}"
       end
     end
   end
+
+  before :deploy, 'rvm1:install:ruby' # install Ruby and create gemset, OR:
+  # before :deploy, 'rvm1:install:gems' # only create gemset
+  before :compile_assets, :link_config_files
+  after :publishing, :restart
 
 end
